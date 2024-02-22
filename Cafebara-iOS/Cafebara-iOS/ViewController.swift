7fee0eed69d38be20aad221e0fda91fbc13e0c44
mcrkgus <mcrkgus@naver.com>
2024-02-22 17:32:47 +0900

    [Setting] #1 - 프로젝트 세팅

---
+++ Cafebara-iOS/Cafebara-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cafebara-iOS
+//
+//  Created by Gahyun Kim on 2024/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
