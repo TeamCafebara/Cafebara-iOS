f19f1a6a0b90b576465c59b12f6fb424de319a79
mcrkgus <mcrkgus@naver.com>
2024-02-23 17:08:08 +0900

    [Setting] #3 - Extension 추가 및 SPM 설치

---
+++ Cafebara-iOS/Cafebara-iOS/Global/Extensions/UIImageView+.swift
@@ -0,0 +1,24 @@
+//
+//  UIImageView+.swift
+//  Cafebara-iOS
+//
+//  Created by Gahyun Kim on 2024/02/23.
+//
+
+import UIKit
+import Kingfisher
+
+extension UIImageView{
+    
+    /// url을 문자열로 넘겨주면 kingfisher를 통해 이미지 설정
+    func kfSetImage(url : String?){
+        guard let url = url else { return }
+        
+        if let url = URL(string: url) {
+            kf.indicatorType = .activity
+            kf.setImage(with: url,
+                        placeholder: nil,
+                        options: [.transition(.fade(1.0))], progressBlock: nil)
+        }
+    }
+}
--- Cafebara-iOS/Cafebara-iOS/Global/Extensions/UIView+.swift
+++ Cafebara-iOS/Cafebara-iOS/Global/Extensions/UIView+.swift
@@ -5,4 +5,20 @@
 //  Created by Gahyun Kim on 2024/02/22.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    /// 한 번에 여러 개의 UIView 또는 UIView의 하위 클래스 객체들을 상위 UIView에 추가
+    func addSubviews(_ views: UIView...) {
+        views.forEach { self.addSubview($0) }
+    }
+    
+    /// UIView 의 모서리 커스텀 - 색상, 모서리 두께, 둥근 정도
+    func setRoundBorder(borderColor: UIColor, borderWidth: CGFloat, cornerRadius: CGFloat) {
+        layer.masksToBounds = true
+        layer.borderColor = borderColor.cgColor
+        layer.borderWidth = borderWidth
+        layer.cornerRadius = cornerRadius
+    }
+}
