978b3f6bea0a96087fd5d7098fb2b16aea6426c8
ahra1221 <lizze011221@gmail.com>
2024-03-08 23:07:57 +0900

    [Feat] #9 - SizeLiterals 구현

--- Cafebara-iOS/Cafebara-iOS/Global/Literals/SizeLiterals.swift
+++ Cafebara-iOS/Cafebara-iOS/Global/Literals/SizeLiterals.swift
@@ -5,4 +5,13 @@
 //  Created by Gahyun Kim on 2024/02/22.
 //
 
-import Foundation
+import UIKit
+
+struct SizeLiterals {
+    
+    struct Screen {
+        static let screenWidth: CGFloat = UIScreen.main.bounds.width
+        static let screenHeight: CGFloat = UIScreen.main.bounds.height
+        static let deviceRatio: CGFloat = screenWidth / screenHeight
+    }
+}
