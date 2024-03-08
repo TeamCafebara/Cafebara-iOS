6fd5ec5eabc69432fc9bc19fa75054b5de1343b9
ahra1221 <lizze011221@gmail.com>
2024-03-09 02:24:58 +0900

    [Feat] #9 - 버튼 state에 따라 배경 설정 함수 구현

--- Cafebara-iOS/Cafebara-iOS/Global/Extensions/UIButton+.swift
+++ Cafebara-iOS/Cafebara-iOS/Global/Extensions/UIButton+.swift
@@ -16,4 +16,21 @@ extension UIButton {
         layer.borderWidth = borderWidth
         layer.borderColor = borderColor.cgColor
     }
+    
+    func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
+        let minimumSize: CGSize = CGSize(width: 1.0, height: 1.0)
+        
+        UIGraphicsBeginImageContext(minimumSize)
+        
+        if let context = UIGraphicsGetCurrentContext() {
+            context.setFillColor(color.cgColor)
+            context.fill(CGRect(origin: .zero, size: minimumSize))
+        }
+        
+        let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        self.clipsToBounds = true
+        self.setBackgroundImage(colorImage, for: state)
+    }
 }
