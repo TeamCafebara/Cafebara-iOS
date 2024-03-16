9187deb87243013857131cc59aa9288fddde1e54
ahra1221 <lizze011221@gmail.com>
2024-03-17 00:03:12 +0900

    [Feat] #12 - viewModel 및 초대코드 로직 구현

---
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Onboarding/InviteCodeScene/ViewController/InviteCodeViewController.swift
@@ -0,0 +1,85 @@
+//
+//  InviteCodeViewController.swift
+//  Cafebara-iOS
+//
+//  Created by 고아라 on 3/14/24.
+//
+
+import UIKit
+
+import Then
+import RxSwift
+import RxCocoa
+
+final class InviteCodeViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let viewModel: OnboardingRegisterViewModel
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - UI Components
+    
+    private let inviteCodeView = InviteCodeView()
+    
+    // MARK: - Life Cycles
+    
+    init(viewModel: OnboardingRegisterViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        
+        view = inviteCodeView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        bindViewModel()
+    }
+}
+
+// MARK: - Extensions
+
+extension InviteCodeViewController {
+
+    func setUI() {
+        self.inviteCodeView.navigationBar.backButtonAction = {
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+
+    func bindViewModel() {
+        inviteCodeView.codeCertifyButton.rx.tap
+            .bind {
+                self.viewModel.inputs.certifyInviteCode(code: self.inviteCodeView.codeInputTextField.text ?? "")
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.outputs.isCertified
+            .subscribe(onNext: { isSuccess in
+                self.inviteCodeView.isCertifyLabel.text = isSuccess ? I18N.OnboardingRegister.isCertifyLabelSuccess : I18N.OnboardingRegister.isCertifyLabelFail
+                self.inviteCodeView.isCertifyLabel.textColor = isSuccess ? .blue75 : .errorBara
+                self.inviteCodeView.isCertifyLabel.isHidden = false
+                self.inviteCodeView.codeInputTextField.textFieldStatus = isSuccess ? .correctType : .uncorrectedType
+                if isSuccess {
+                    self.inviteCodeView.codeCertifyButton.do {
+                        $0.isEnabled = false
+                        $0.setBackgroundColor(.blue10, for: .disabled)
+                        $0.layer.borderColor = UIColor.blue25.cgColor
+                        $0.setTitleColor(.blue25, for: .disabled)
+                    }
+                    self.inviteCodeView.codeInputTextField.isUserInteractionEnabled = false
+                    self.inviteCodeView.nextButton.isEnabled = true
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+}
--- Cafebara-iOS/Cafebara-iOS/Presentation/Onboarding/RoleChoiceScene/ViewModel/OnboardingRegisterViewModel.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Onboarding/RoleChoiceScene/ViewModel/OnboardingRegisterViewModel.swift
@@ -14,10 +14,11 @@ import Moya
 protocol OnboardingRegisterViewModelInputs {
     func saveRole(isOwner: Bool)
     func saveName(name: String)
+    func certifyInviteCode(code: String)
 }
 
 protocol OnboardingRegisterViewModelOutputs {
-
+    var isCertified: PublishSubject<Bool> { get }
 }
 
 protocol OnboardingRegisterViewModelType {
@@ -44,11 +45,21 @@ final class OnboardingRegisterViewModel: OnboardingRegisterViewModelInputs, Onbo
         self.name = name
     }
     
+    func certifyInviteCode(code: String) {
+        print("✅✅✅입력코드✅✅✅")
+        print(code)
+        print("✅✅✅입력코드✅✅✅")
+        isCertified.onNext(true)
+    }
+    
+    // output
+    
+    var isCertified: PublishSubject<Bool> = PublishSubject<Bool>()
+    
     init() {
 
     }
 }
 
 extension OnboardingRegisterViewModel {
-    
 }
