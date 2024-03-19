0ca5a805466f034161a1f7f37548ef4a84b39a1c
Minsoo Kang <alstn38@naver.com>
2024-03-19 22:24:51 +0900

    [Feat] #14 - MyWorkInfo Model 구현

---
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Home/MyWorkScene/Model/MyWorkInfo.swift
@@ -0,0 +1,18 @@
+//
+//  MyWorkInfo.swift
+//  Cafebara-iOS
+//
+//  Created by 강민수 on 3/19/24.
+//
+
+import Foundation
+
+struct MyWorkInfo {
+    let date: String
+    let workKeyword: String
+    let workKeywordTextColor: String
+    let workKeywordBackColor: String
+    let startTime: String
+    let endTiem: String
+    let name: String
+}
