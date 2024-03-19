eb6aa04317215217c08678ea5b98252d6ff88fe4
Minsoo Kang <alstn38@naver.com>
2024-03-19 22:52:36 +0900

    [Feat] #14 - 나의 근무 ViewController 구현

--- Cafebara-iOS/Cafebara-iOS/Presentation/Home/MyWorkScene/ViewController/MyWorkViewController.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Home/MyWorkScene/ViewController/MyWorkViewController.swift
@@ -7,18 +7,23 @@
 
 import UIKit
 
+import RxSwift
+import RxCocoa
+
 final class MyWorkViewController: UIViewController {
     
     // MARK: - Properties
-    
+    private let myWorkViewModel = MyWorkViewModel()
+    private let disposeBag = DisposeBag()
     
     // MARK: - UI Components
     
+    private let myWorkView = MyWorkView()
     
     // MARK: - Life Cycles
     
     override func loadView() {
-        
+        view = myWorkView
     }
     
     override func viewDidLoad() {
@@ -35,11 +40,54 @@ final class MyWorkViewController: UIViewController {
 extension MyWorkViewController {
 
     func setUI() {
-        
+        self.navigationController?.navigationBar.isHidden = true
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+        myWorkView.navigationBar.backButtonAction = {
+            self.navigationController?.popViewController(animated: false)
+        }
     }
 
     func bindViewModel() {
-	
+        myWorkViewModel.myWorkInfoObservable
+            .map { $0.date }
+            .bind(to: myWorkView.myWorkInfoView.dateLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        myWorkViewModel.myWorkInfoObservable
+            .map { $0.workKeyword }
+            .bind(to: myWorkView.myWorkInfoView.workKeywordLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        myWorkViewModel.myWorkInfoObservable
+            .map { $0.workKeywordTextColor }
+            .map { hexString in
+                UIColor(hex: hexString, alpha: 1.0)
+            }
+            .bind(to: myWorkView.myWorkInfoView.workKeywordLabel.rx.textColor)
+            .disposed(by: disposeBag)
+        
+        myWorkViewModel.myWorkInfoObservable
+            .map { $0.workKeywordBackColor }
+            .map { hexString in
+                UIColor(hex: hexString, alpha: 1.0)
+            }
+            .bind(to: myWorkView.myWorkInfoView.workKeywordLabel.rx.backgroundColor)
+            .disposed(by: disposeBag)
+        
+        myWorkViewModel.myWorkInfoObservable
+            .map { $0.startTime }
+            .bind(to: myWorkView.myWorkInfoView.workStartTimeLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        myWorkViewModel.myWorkInfoObservable
+            .map { $0.endTiem }
+            .bind(to: myWorkView.myWorkInfoView.workEndTimeLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        myWorkViewModel.myWorkInfoObservable
+            .map { $0.name }
+            .bind(to: myWorkView.myWorkInfoView.workerNameLabel.rx.text)
+            .disposed(by: disposeBag)
     }
     
     func setDelegate() {
