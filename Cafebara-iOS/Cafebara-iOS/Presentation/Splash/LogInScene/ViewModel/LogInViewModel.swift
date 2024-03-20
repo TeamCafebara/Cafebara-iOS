c085470f19430165bef2a721295bdcd48a6bfea7
bangMinji98 <whoru9806@gmail.com>
2024-03-20 20:52:11 +0900

    [Feat] #13 로그인뷰모델 구현

--- Cafebara-iOS/Cafebara-iOS/Presentation/Splash/LogInScene/ViewModel/LogInViewModel.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Splash/LogInScene/ViewModel/LogInViewModel.swift
@@ -5,4 +5,38 @@
 //  Created by 방민지 on 3/13/24.
 //
 
-import Foundation
+import UIKit
+
+import RxSwift
+import RxCocoa
+import Moya
+
+protocol LoginViewModelInputs {
+}
+
+protocol LoginViewModelOutputs {
+    
+    var imageData: BehaviorRelay<[LoginImageDto]> { get }
+}
+
+protocol LoginViewModelType {
+    
+    var inputs: LoginViewModelInputs { get }
+    var outputs: LoginViewModelOutputs { get }
+}
+
+final class LoginViewModel: LoginViewModelInputs, LoginViewModelOutputs, LoginViewModelType {
+    
+    var inputs: LoginViewModelInputs { return self }
+    var outputs: LoginViewModelOutputs { return self }
+ 
+    // output
+    
+    var imageData: BehaviorRelay<[LoginImageDto]> = BehaviorRelay<[LoginImageDto]>(value: LoginImageDto.imageDummy())
+    
+    init() {
+    }
+}
+
+extension LoginViewModel {
+}
