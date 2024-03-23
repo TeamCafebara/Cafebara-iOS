af53c3e0b09354ce44952bcb45ae9c55f3aab5c5
Minsoo Kang <alstn38@naver.com>
2024-03-23 12:13:02 +0900

    [Fix] #14 - 나의 근무 ViewModel 수정

--- Cafebara-iOS/Cafebara-iOS/Presentation/Home/MyWorkScene/ViewModel/MyWorkViewModel.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Home/MyWorkScene/ViewModel/MyWorkViewModel.swift
@@ -15,7 +15,7 @@ protocol MyWorkViewModelInputs {
 }
 
 protocol MyWorkViewModelOutputs {
-    
+    var myWorkInfoData: BehaviorSubject<MyWorkInfo> { get }
 }
 
 protocol MyWorkViewModelType {
@@ -28,23 +28,29 @@ final class MyWorkViewModel: MyWorkViewModelInputs, MyWorkViewModelOutputs, MyWo
     var inputs: MyWorkViewModelInputs { return self }
     var outputs: MyWorkViewModelOutputs { return self }
     
-    let myWorkInfoObservable = BehaviorSubject<MyWorkInfo>(value: MyWorkInfo(date: "02.14 수요일",
-                                                                             workKeyword: "오픈",
-                                                                             workKeywordTextColor: "#1F9BB6",
-                                                                             workKeywordBackColor: "#EAFBFA",
-                                                                             startTime: "9:00",
-                                                                             endTiem: "13:00",
-                                                                             name: "강민수"))
+    // TODO: 서버 통신시 PublishSubject로 변경하기
+    var myWorkInfoData = BehaviorSubject<MyWorkInfo>(value: MyWorkInfo(
+        date: "02.14 수요일",
+        workKeyword: "오픈",
+        workKeywordTextColor: "#1F9BB6",
+        workKeywordBackColor: "#EAFBFA",
+        startTime: "9:00",
+        endTiem: "13:00",
+        name: "강민수"
+    ))
+    
+    
     
     init() {
-        let myWorkInfo: MyWorkInfo = MyWorkInfo(date: "02.14 수요일", 
+        // TODO: 서버 통신 받기 이전 DTO
+        let myWorkInfo: MyWorkInfo = MyWorkInfo(date: "02.14 수요일",
                                                 workKeyword: "오픈",
                                                 workKeywordTextColor: "#1F9BB6",
                                                 workKeywordBackColor: "#EAFBFA",
                                                 startTime: "9:00",
                                                 endTiem: "13:00",
                                                 name: "강민수")
-        myWorkInfoObservable.onNext(myWorkInfo)
+        myWorkInfoData.onNext(myWorkInfo)
     }
 }
 
