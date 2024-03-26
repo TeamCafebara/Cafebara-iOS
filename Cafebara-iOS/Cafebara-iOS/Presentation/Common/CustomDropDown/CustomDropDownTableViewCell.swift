973dd634acf5b993473bca34d617ffc80d0b5c60
Minsoo Kang <alstn38@naver.com>
2024-03-26 21:58:36 +0900

    [Fix] #14 - CustomDropDown cell 재사용 수정

--- Cafebara-iOS/Cafebara-iOS/Presentation/Common/CustomDropDown/CustomDropDownTableViewCell.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Common/CustomDropDown/CustomDropDownTableViewCell.swift
@@ -30,6 +30,13 @@ final class CustomDropDownTableViewCell: UITableViewCell, UITableViewRegisterabl
         setLayout()
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        self.backgroundColor = .whiteBara
+        dropDownItemLabel.textColor = .gray7
+    }
+    
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
